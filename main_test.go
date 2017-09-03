68edf14dd2a2dd3c1f68af65c1b00c2fc10727c9
William Stewart <zoidbergwill@gmail.com>
2017-09-03 15:48:24 +0200

    Test Message Handler

--- main_test.go
+++ main_test.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"testing"
+	"net/http/httptest"
+	"net/http"
+	"bytes"
 )
 
 type MessageStream struct {
@@ -27,5 +30,51 @@ func TestSchema(t *testing.T) {
 	for _, message := range stream.Items {
 		fmt.Printf("%d: %d\n", message.Provider.Appid, message.Provider.Timestamp)
 	}
+}
 
+func TestMessageHandler(t *testing.T) {
+	file, err := ioutil.ReadFile("./samples.json")
+	if err != nil {
+		t.Error("File error: %v\n", err)
+	}
+
+	var stream MessageStream
+	err = json.Unmarshal(file, &stream)
+	if err != nil {
+		t.Error("JSON error: %v\n", err)
+	}
+
+	//fmt.Printf("%v\n", stream.Items)
+	for _, message := range stream.Items {
+		// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+		// pass 'nil' as the third parameter.
+		body, err := json.Marshal(message)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req, err := http.NewRequest("POST", "/", bytes.NewReader(body))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(MessageHandler)
+
+		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+		// directly and pass in our Request and ResponseRecorder.
+		handler.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		expected := `Message Received!`
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
+		}
+	}
 }
